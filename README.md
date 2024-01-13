0a88eaba6e11c951ad9ef7167410408001851ce1
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 17:27:14 +0530

    Terraform-databases backend,docdb,statefile share

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Terraform-module-docdb
+
+This is the backend module to Terraform-databases
+
+All the code that's needed to provision document-db will be placed in this particaular repository
---
+++ docdb.tf
@@ -0,0 +1,24 @@
+# Provisions Document-DB Cluster
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "roboshop-${var.ENV}-docdb"
+  engine                  = "docdb"
+  master_username         = "admin1"
+  master_password         = "roboshop1"
+#   backup_retention_period = 5         # Commented to avoid backup
+#   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.docdb.name
+}
+
+
+# Creates a subnet-groups
+
+resource "aws_docdb_subnet_group" "docdb" {
+  name       = "roboshop-${var.ENV}-docdb-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-${var.ENV}-docdb-subnet-group"
+  }
+}
