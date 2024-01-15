ae9611cf5dece2f8e01bf2fcd53b0f9aaa7d37dd
Adnan <adnanshaikh11501@gmail.com>
2024-01-15 13:00:56 +0530

    DOCDB  schema setup

--- schema.tf
+++ schema.tf
@@ -4,6 +4,7 @@ resource "null_resource" "schema" {
 
   provisioner "local-exec" {
     command = <<EOF
+        cd /tmp
         wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
         curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
         unzip -o mongodb.zip
